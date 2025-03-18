6c58faa74599479d525f143724ce4709baa4c682
Ivan Povazan <ivan.povazan@gmail.com>
2025-03-18 11:40:14 +0100

    Add a check before building

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 source "$(dirname "$0")/init.sh"
 
+if [ ! -f "$LOCAL_DOTNET" ]; then
+    echo "Error: $LOCAL_DOTNET does not exist. Please run ./prepare.sh first."
+    exit 1
+fi
+
 if [ -z "$1" ]; then
     echo "Usage: $0 <dotnet-new-android|dotnet-new-maui|dotnet-new-maui-samplecontent>"
     exit 1

ba7e44ea322dd8030df63c742e54695afd4c2808
Ivan Povazan <ivan.povazan@gmail.com>
2025-03-18 10:51:07 +0100

    Adding dotnet new maui

---
+++ dotnet-new-maui/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace dotnet_new_maui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
---
+++ dotnet-new-maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_new_maui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
